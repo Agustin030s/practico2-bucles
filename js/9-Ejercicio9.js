880ff733aa27408f49468c84e253de117423b600
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-25 14:11:18 -0300

    se realizo el ejercicio 9

---
+++ js/9-Ejercicio9.js
@@ -0,0 +1,20 @@
+/*
+Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal.
+*/
+
+for(let i = 1; i <= 500; i++){
+
+    if(i % 4 === 0 && i % 9 === 0){
+        document.write(`<h2 class="ms-4">${i} (Múltiplo de 4 y 9)</h2>`);
+    } else if( i % 4 === 0){
+        document.write(`<h2 class="ms-4">${i} (Múltiplo de 4)</h2>`)
+    } else if( i % 9 === 0){
+        document.write(`<h2 class="ms-4">${i} (Múltiplo de 9)</h2>`);
+    }else{
+        document.write(`<h2 class="ms-4">${i}</h2>`);
+    }
+
+    if( i % 5 === 0){
+        document.write("<hr>");
+    }
+}
