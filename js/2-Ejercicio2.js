c41e77c12aed6ec165812d1ef7906482eebfcf5c
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-25 12:06:57 -0300

    se realizo el ejercicio 1 y 2

---
+++ js/2-Ejercicio2.js
@@ -0,0 +1,28 @@
+/*
+Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+*/
+
+let nota = parseInt(prompt("Ingrese una nota"));
+
+if(nota < 0 || nota > 10){
+    alert("La nota ingresada no es valida");
+} else if( nota >= 0 && nota <= 2){
+    alert(`Su nota es ${nota}: Muy Deficiente`);
+} else if( nota >= 3 && nota <= 4){
+    alert(`Su nota es ${nota}: Insuficiente`);
+}else if(nota >= 5 && nota <= 6){
+    alert(`Su nota es ${nota}: Suficiente`);
+}else if( nota === 7){
+    alert(`Su nota es ${nota}: Buena`);
+} else if( nota >= 8 && nota <= 9){
+    alert(`Su nota es ${nota}: Notable`);
+}else{
+    alert(`Su nota es ${nota}: Sobresaliente`);
+}
