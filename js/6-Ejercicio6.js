22c3b73ad16065f5a227954c8fe9aa87432951fe
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-25 13:46:07 -0300

    se realizo el ejercicio 6 con éxito

---
+++ js/6-Ejercicio6.js
@@ -0,0 +1,21 @@
+/*
+Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+……
+*/
+
+for(let i = 1; i <= 30; i++){
+    
+    let linea = "";
+    for(let j = 1; j <= i; j++){
+        linea += i;
+    }
+
+    document.write(`<h2 class="ms-4">${linea}</h2>`);
+}
