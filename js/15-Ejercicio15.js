546db04538c840c83292ef820342f514e684a4c1
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-25 15:06:45 -0300

    se realizo el ejercicio 17

--- js/15-Ejercicio15.js
+++ js/15-Ejercicio15.js
@@ -3,9 +3,10 @@ Realiza un script que cuente el número de vocales que tiene un texto.
 */
 
 let texto = prompt("Ingrese un texto");
+texto = texto.toLowerCase();
 let contador = 0;
 
-for (let i = 1; i <= texto.length; i++) {
+for (let i = 0; i < texto.length; i++) {
   if (
     texto.charAt(i) === "a" ||
     texto.charAt(i) === "e" ||
---
+++ js/17-Ejercicio17.js
@@ -0,0 +1,22 @@
+/*
+Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
+*/
+
+let texto = prompt("Ingrese un texto");
+texto = texto.toLowerCase();
+
+let posición = 0;
+
+for(let i = 0; i <= texto.length; i++){
+    if (
+        texto.charAt(i) === "a" ||
+        texto.charAt(i) === "e" ||
+        texto.charAt(i) === "i" ||
+        texto.charAt(i) === "o" ||
+        texto.charAt(i) === "u"
+      ){
+        posición = i;
+        document.write(`<h2 class="ms-4">La vocal "${texto.charAt(i)}" está en la posición ${i}</h2>`);
+        break;
+      }
+}
